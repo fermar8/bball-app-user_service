b04faeba3fa967198b0794bd703040530783d236
Ferran Martínez Salcedo <ferran.ms8@gmail.com>
2026-04-28 14:29:12 +0200

    fix pipeline for scaffolding

--- terraform/resources/ec2.tf
+++ terraform/resources/ec2.tf
@@ -39,7 +39,7 @@ resource "aws_instance" "app_server" {
 
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 20
+    volume_size           = 30
     delete_on_termination = true
     encrypted             = true
   }
